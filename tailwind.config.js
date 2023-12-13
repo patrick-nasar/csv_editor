e3356f4d2cb90cdd01b188f42c290db29b229674
Patrick <patriknasar@gmail.com>
2023-12-13 12:46:23 +0100

    fadeout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        wiggle: 'wiggle 200ms linear',
+        fade: 'fadeOut 1.5s linear',
+        slide_left: 'slide_left 2s ease-in-out',
+        slide_right: 'slide_right 2s ease-in-out',
+      },
+
+      keyframes: theme => ({
+        fadeOut: {
+          '0%': { opacity: 0 },
+          '50%': { opacity: 0.7 },
+          '100%': { opacity: 1 },
+        },
+
+        slide_left: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        slide_right: {
+          '0%': { transform: 'translateX(50%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+
+      }),
+    },
   },
   plugins: [
   ],
